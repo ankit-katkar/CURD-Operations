5ac7419e642e145cb1ae559adfdb3a7333ffee38
Ankit Katkar <ankitkatkar8@gmail.com>
2023-07-30 10:03:59 +0530

    code push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Simple-CURD-Operaction';
+  name='';
+  email='';
+  number='';
+
+  isEdit=false;
+  editIndex='';
+  userData:any=[];
+  inputData:any=[];
+
+
+  onSubmit(){
+ let inputData ={
+  name:this.name,
+  email:this.email,
+  number:this.number
+ }
+this.userData.push(inputData)
+this.clear();
+}
+
+edit(i:any){
+  this.editIndex = i;
+  this.name=this.userData[i].name;
+  this.email=this.userData[i].email;
+  this.number= this.userData[i].number
+
+  this.isEdit = true;
+}
+ 
+onUpdate(){
+this.isEdit = false;
+
+let inputData ={
+  name:this.name,
+  email:this.email,
+  number:this.number
+ }
+
+ this.userData[this.editIndex]=inputData;
+ this.clear();
+
+}
+delete(i:any){
+  this.userData.splice(i,1)
+}
+
+  clear() { 
+    this.name = '';
+    this.email = '';
+    this.number = '';
+  }
+
 }
